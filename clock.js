6c55f99e4d90804c94574b0f53c0cbefeb3952a6
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-05 22:04:46 -0400

    comments based on instruction

--- clock.js
+++ clock.js
@@ -22,7 +22,7 @@ function tickClock() {
   clockSection.textContent = getTime()
 }
 
-//call tickClock as page loads, let setInterval do it every second after
+//call tickClock as page loads, let setInterval do it every second after because setInterval does not start until after delay
 tickClock()
 
 //use setInterval function to execute clock every second
@@ -30,4 +30,9 @@ tickClock()
 //setInterval(tickClock, 1000)
 
 //test if you can use an anonymous arrow function in place of tickClock in setInterval
+//instructor did not say remove tickClock function in instructions
+//argues that doing so stops initial call above, so page loads with "insert clock here" in place of time
+//says can, but shouldn't
 setInterval(() => (clockSection.textContent = getTime()), 1000)
+//I agree, it seems stupid to write a clear function above then remove it to use an anonymous function.
+//I would argue however, that the problem isn't removing the original function, it's attempting use an anonymous function to see if we can
